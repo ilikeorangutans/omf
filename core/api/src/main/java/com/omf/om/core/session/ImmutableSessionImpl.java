bd4c938eb1dd72b860c34ee3cbc501b4ca0984f8
Jakob Külzer <jakobk@gmail.com>
2012-03-10 16:50:13 -0500

    Formatted files

--- core/api/src/main/java/com/omf/om/core/session/ImmutableSessionImpl.java
+++ core/api/src/main/java/com/omf/om/core/session/ImmutableSessionImpl.java
@@ -14,8 +14,6 @@ public class ImmutableSessionImpl implements Session {
 	public <T> T get(Class<T> clazz, String path) {
 		if (clazz == null)
 			throw new NullPointerException("Class is null.");
-		
-		
 
 		return null;
 	}
