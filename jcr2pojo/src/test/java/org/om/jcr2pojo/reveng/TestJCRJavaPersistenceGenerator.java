96762eaa27f89ecb217f968f0f6094324fb86cda
Tom Everett <tome@criticalmass.com>
2012-03-18 15:00:41 -0600

    more xml tests
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- jcr2pojo/src/test/java/org/om/jcr2pojo/reveng/TestJCRJavaPersistenceGenerator.java
+++ jcr2pojo/src/test/java/org/om/jcr2pojo/reveng/TestJCRJavaPersistenceGenerator.java
@@ -7,7 +7,6 @@ import javax.jcr.Session;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.om.core.impl.persistence.jcr.impl.entitymappingbuilder.namingstrategy.DefaultPropertyNamingStrategy;
 import org.om.core.impl.persistence.jcr.impl.entitymappingbuilder.namingstrategy.NodeNameClassNamingStrategy;
@@ -21,31 +20,28 @@ import org.om.core.impl.persistence.jcr.util.ImportUtil;
  */
 public class TestJCRJavaPersistenceGenerator {
 
-	@Before
-	public void setUp() {
-		try {
-			final InputStream is = TestJCRJavaPersistenceGenerator.class.getResourceAsStream("/test.xml");
-			ImportUtil.importXML("test", is);
-		} catch (final Exception e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
-	}
-
 	@Test
 	public void test1() {
 		try {
+			/*
+			 * import some data
+			 */
+			final InputStream is = TestJCRJavaPersistenceGenerator.class.getResourceAsStream("/test.xml");
+			ImportUtil.importXML("test", is);
+			/*
+			 * get a session
+			 */
 			final Session session = new PropertiesConfiguredJCRSessionFactory().getSession();
 			Assert.assertNotNull(session);
 			/*
-			 * get the root node
+			 * get the node
 			 */
-			final Node rootNode = session.getRootNode();
+			final Node rootNode = session.getRootNode().getNode("test");
 			Assert.assertNotNull(rootNode);
 			/*
 			 * go for it
 			 */
-			final JCRJavaPersistenceGenerator engine = new JCRJavaPersistenceGenerator(rootNode, "com.khubla.revenge.test", "target/reveng",
+			final JCRJavaPersistenceGenerator engine = new JCRJavaPersistenceGenerator(rootNode, "com.khubla.revenge.test", "target/reveng/test1",
 					new NodeNameClassNamingStrategy(), new DefaultPropertyNamingStrategy());
 			engine.execute();
 		} catch (final Exception e) {
@@ -54,4 +50,36 @@ public class TestJCRJavaPersistenceGenerator {
 		}
 
 	}
+
+	@Test
+	public void test2() {
+		try {
+			/*
+			 * import some data
+			 */
+			final InputStream is = TestJCRJavaPersistenceGenerator.class.getResourceAsStream("/alfresco.xml");
+			ImportUtil.importXML("alfresco", is);
+			/*
+			 * get a session
+			 */
+			final Session session = new PropertiesConfiguredJCRSessionFactory().getSession();
+			Assert.assertNotNull(session);
+			/*
+			 * get the node
+			 */
+			final Node rootNode = session.getRootNode().getNode("alfresco");
+			Assert.assertNotNull(rootNode);
+			/*
+			 * go for it
+			 */
+			final JCRJavaPersistenceGenerator engine = new JCRJavaPersistenceGenerator(rootNode, "com.khubla.revenge.test", "target/reveng/test2",
+					new NodeNameClassNamingStrategy(), new DefaultPropertyNamingStrategy());
+			engine.execute();
+		} catch (final Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+
+	}
+
 }
