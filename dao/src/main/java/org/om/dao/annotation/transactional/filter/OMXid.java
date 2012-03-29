7d4f62a76fcb356e205ad6139984cfec0714d898
Tom Everett <tome@criticalmass.com>
2012-03-29 17:03:54 -0600

    @Transactional implemented with XA transactions
    Signed-off-by: Tom Everett <tome@criticalmass.com>

---
+++ dao/src/main/java/org/om/dao/annotation/transactional/filter/OMXid.java
@@ -0,0 +1,25 @@
+package org.om.dao.annotation.transactional.filter;
+
+import javax.transaction.xa.Xid;
+
+/**
+ * OM Xid
+ * 
+ * @author tome
+ * 
+ */
+public class OMXid implements Xid {
+
+	public byte[] getBranchQualifier() {
+		return new byte[0];
+	}
+
+	public int getFormatId() {
+		return 0;
+	}
+
+	public byte[] getGlobalTransactionId() {
+		return new byte[0];
+	}
+
+}
--- dao/src/main/java/org/om/dao/util/SessionUtil.java
+++ dao/src/main/java/org/om/dao/util/SessionUtil.java
@@ -2,6 +2,7 @@ package org.om.dao.util;
 
 import org.om.core.api.session.Session;
 import org.om.core.api.session.factory.SessionFactory;
+import org.om.core.impl.persistence.jcr.JcrPersistenceContext;
 import org.om.core.impl.persistence.jcr.exception.JCRException;
 import org.om.dao.config.ObjectManagerConfiguration;
 import org.om.dao.config.impl.XMLObjectManagerConfiguration;
@@ -36,6 +37,24 @@ public class SessionUtil {
 			 */
 			return sessionFactory.getSession(objectManagerConfiguration.getJcrPersistenceContext());
 
+		} catch (final Exception e) {
+			throw new JCRException("Exception in getSession", e);
+		}
+	}
+
+	/**
+	 * get a JCR context
+	 */
+	public static JcrPersistenceContext getJcrPersistenceContext() throws JCRException {
+		try {
+			/*
+			 * init if needed
+			 */
+			init();
+			/*
+			 * session, via the JCR context
+			 */
+			return objectManagerConfiguration.getJcrPersistenceContext();
 		} catch (final Exception e) {
 			throw new JCRException("Exception in getSession", e);
 		}
