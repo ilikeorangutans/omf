5ae23c0331c37c6fc388fb8b9b55aeeba681ddc8
Jakob Külzer <jakob.kuelzer@gmail.com>
2012-08-27 23:22:30 -0400

    Added missing license headers, some more javadocs

--- omf-api/src/main/java/org/om/core/api/exception/MissingException.java
+++ omf-api/src/main/java/org/om/core/api/exception/MissingException.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright 2012 Jakob Külzer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.om.core.api.exception;
 
+/**
+ * Exception thrown when a mapped property could not be resolved.
+ * 
+ * TODO: Isn't this the same as {@link PathNotFoundException}?
+ * 
+ * @author Jakob Külzer
+ * 
+ */
 public class MissingException extends ObjectMapperException {
 
 	private static final long serialVersionUID = 1L;
